10c594a12ea9b4f786db6e3faf8f2e7037327905
Matt Robold <mrobold@gmail.com>
2023-01-06 02:25:01 -0800

    Replaced nested constant strings with SmartEnums. Wrestled with bson serialization until it finally admitted that I just needed to set a constructor in the global mapper. Tests now utilize LiteDb. (#3)

--- src/Muddlr.Core/WebFinger/WebFingerLink.cs
+++ src/Muddlr.Core/WebFinger/WebFingerLink.cs
@@ -1,12 +1,16 @@
-using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Ardalis.SmartEnum.SystemTextJson;
 
 namespace Muddlr.WebFinger;
 
 public class WebFingerLink
 {
-    [Required]
-    public string Relationship { get; set; }
-    public string? Type { get; set; }
+    [JsonConverter(typeof(SmartEnumValueConverter<Relationship, string>))]
+    public Relationship Relationship { get; set; }
+    
+    [JsonConverter(typeof(SmartEnumValueConverter<LinkType, string?>))]
+    public LinkType? Type { get; set; }
+    
     public Uri? Href { get; set; }
     public Dictionary<string, string>? Titles { get; set; }
     public string? Template { get; set; }
